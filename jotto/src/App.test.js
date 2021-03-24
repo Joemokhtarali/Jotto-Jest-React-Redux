47a8adc8284d6915d3b7c863ba12012878a0a4ac
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2021-03-24 17:26:03 -0400

    initial commit

--- jotto/src/App.test.js
+++ jotto/src/App.test.js
@@ -1,4 +1,6 @@
 import { render, screen } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme'
+import EnzymeAdaptor from '@wojtekmaj/enzyme-adapter-react-17'
 import App from './App';
 
 test('renders learn react link', () => {
