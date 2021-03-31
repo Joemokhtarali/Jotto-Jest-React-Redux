7c4a06b759662ac769e99276148d2dc217d790ec
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2021-03-31 17:41:03 -0400

    added a test for a controlled form- onChange test passed

--- jotto/src/Input.js
+++ jotto/src/Input.js
@@ -1,14 +1,33 @@
 import React from "react";
-import propTypes from 'prop-types'
+import propTypes from "prop-types";
 
 const Input = ({ secretWord }) => {
-  return <div data-test="component-input">
-
-  </div>;
+    const [currentGuess, setCurrentGuess] = React.useState('')
+  return (
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          type="text"
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          value={currentGuess}
+          onChange={(event) => setCurrentGuess(event.target.value)}
+          placeholder="enter guess"
+        />
+        <button
+          type="submit"
+          data-test="submit-button"
+          className="btn btn-promary mb-2"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 Input.propTypes = {
-    secretWord: propTypes.string.isRequired
-}
+  secretWord: propTypes.string.isRequired,
+};
 
 export default Input;
