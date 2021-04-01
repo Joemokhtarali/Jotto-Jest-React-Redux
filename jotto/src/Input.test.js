b61b846063c2ab3d5ed0eea5d1799914f3eddfa4
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2021-04-01 11:55:43 -0400

    render test for success attr show input & button or not

--- jotto/src/Input.test.js
+++ jotto/src/Input.test.js
@@ -5,15 +5,50 @@ import { findByTestAttr, checkProps } from '../test/testUtils.js'
 import Input from './Input'
 
 
-const setup = (secretWord='party') => {
-    return shallow(<Input secretWord={secretWord}/>)
+const setup = (success=false, secretWord='party') => {
+    return shallow(<Input success={success} secretWord={secretWord}/>)
 }
 
-test('renders without error', () => {
-    const wrapper = setup()
-    const inputComponent = findByTestAttr(wrapper, 'component-input')
-    expect(inputComponent.length).toBe(1)
+describe('render', () => {
+    describe('success is true', () => {
+        let wrapper
+        beforeEach(() => {
+            wrapper = setup(true)
+        })
+        test('renders without error', () => {
+            const inputComponent = findByTestAttr(wrapper, 'component-input')
+            expect(inputComponent.length).toBe(1)
+        })
+        test('input box does not show', () => {
+            const inputBox = findByTestAttr(wrapper, 'input-box')
+            expect(inputBox.exists()).toBe(false)
+        })
+        test('submit botton does not show', () => {
+            const submitButton = findByTestAttr(wrapper, 'submit-button')
+            expect(submitButton.exists()).toBe(false)
+        })
+    })
+    describe('success is false', () => {
+        let wrapper
+        beforeEach(() => {
+            wrapper = setup(false)
+        })
+        test('renders without error', () => {
+            const inputComponent = findByTestAttr(wrapper, 'component-input')
+            expect(inputComponent.length).toBe(1)
+        })
+        test('input box shows', () => {
+            const inputBox = findByTestAttr(wrapper, 'input-box')
+            expect(inputBox.exists()).toBe(true)
+        })
+        test('submit botton shows', () => {
+            const submitButton = findByTestAttr(wrapper, 'submit-button')
+            expect(submitButton.exists()).toBe(true)
+        })
+    })
 })
+
+
 
 test('does not throw warning with expected props', () => {
     checkProps(Input, {secretWord: 'party'})
